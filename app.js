0ec8542af2d74e52592ab88f91d4f79662d4b087
JulioD <ddaavviidd.duran@gmail.com>
2024-02-07 12:19:40 -0400

    web index agregado

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(cors({
       'http://localhost:8080',
       'http://localhost:1234',
       'https://movies.com',
-      'https://midu.dev'
+      'https://midu.dev',
+      'http://127.0.0.1:5500'
     ]
 
     if (ACCEPTED_ORIGINS.includes(origin)) {
